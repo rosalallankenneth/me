7de9175c5983adb0747fc8cc8908e361ebb1e311
rosalallankenneth <113879316+rosalallankenneth@users.noreply.github.com>
2022-09-23 17:09:48 +0000

    Deploying to gh-pages from @ rosalallankenneth/me@5118742ab519e74db164253f1a99afcc27a9cce2 🚀

---
+++ skills.json.js
@@ -0,0 +1,152 @@
+export const skillsStyle = [
+  {
+    id: 1,
+    path: "/images/skills/html.png",
+    alt: "html logo",
+    label: "HTML"
+  },
+  {
+    id: 2,
+    path: "/images/skills/css.png",
+    alt: "css logo",
+    label: "CSS"
+  },
+  {
+    id: 3,
+    path: "/images/skills/tailwind.png",
+    alt: "tailwind css logo",
+    label: "Tailwind CSS"
+  },
+  {
+    id: 4,
+    path: "/images/skills/material.png",
+    alt: "material ui logo",
+    label: "Material UI"
+  },
+  {
+    id: 5,
+    path: "/images/skills/bootstrap.png",
+    alt: "bootstrap logo",
+    label: "Bootstrap"
+  },
+  {
+    id: 6,
+    path: "/images/skills/html.png",
+    alt: "html logo",
+    label: "HTML"
+  },
+  {
+    id: 7,
+    path: "/images/skills/css.png",
+    alt: "css logo",
+    label: "CSS"
+  },
+  {
+    id: 8,
+    path: "/images/skills/tailwind.png",
+    alt: "tailwind css logo",
+    label: "Tailwind CSS"
+  }
+];
+
+export const skillsJs = [
+  {
+    id: 1,
+    path: "/images/skills/js.png",
+    alt: "javascript logo",
+    label: "Javascript"
+  },
+  {
+    id: 2,
+    path: "/images/skills/jquery.png",
+    alt: "jquery logo",
+    label: "JQuery"
+  },
+  {
+    id: 3,
+    path: "/images/skills/react.png",
+    alt: "react js logo",
+    label: "React"
+  },
+  {
+    id: 4,
+    path: "/images/skills/next.png",
+    alt: "next js logo",
+    label: "Next JS"
+  },
+  {
+    id: 5,
+    path: "/images/skills/node.png",
+    alt: "node js logo",
+    label: "Node JS"
+  },
+  {
+    id: 6,
+    path: "/images/skills/js.png",
+    alt: "javascript logo",
+    label: "Javascript"
+  },
+  {
+    id: 7,
+    path: "/images/skills/jquery.png",
+    alt: "jquery logo",
+    label: "JQuery"
+  },
+  {
+    id: 8,
+    path: "/images/skills/react.png",
+    alt: "react js logo",
+    label: "React"
+  }
+];
+
+export const skillsBackend = [
+  {
+    id: 1,
+    path: "/images/skills/redux.png",
+    alt: "redux logo",
+    label: "Redux"
+  },
+  {
+    id: 2,
+    path: "/images/skills/firebase.png",
+    alt: "firebase logo",
+    label: "Firebase"
+  },
+  {
+    id: 3,
+    path: "/images/skills/php.png",
+    alt: "php logo",
+    label: "PHP"
+  },
+  {
+    id: 4,
+    path: "/images/skills/mysql.png",
+    alt: "mysql logo",
+    label: "MySQL"
+  },
+  {
+    id: 5,
+    path: "/images/skills/graphql.png",
+    alt: "graphql logo",
+    label: "GraphQL"
+  },
+  {
+    id: 6,
+    path: "/images/skills/redux.png",
+    alt: "redux logo",
+    label: "Redux"
+  },
+  {
+    id: 7,
+    path: "/images/skills/firebase.png",
+    alt: "firebase logo",
+    label: "Firebase"
+  },
+  {
+    id: 8,
+    path: "/images/skills/php.png",
+    alt: "php logo",
+    label: "PHP"
+  }
+];
