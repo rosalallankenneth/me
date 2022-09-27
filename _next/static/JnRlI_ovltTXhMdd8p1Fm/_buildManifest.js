1d6cbda29bca7c6890f7174595ecdeb36a29aacc
rosalallankenneth <113879316+rosalallankenneth@users.noreply.github.com>
2022-09-27 15:47:32 +0000

    Deploying to gh-pages from @ rosalallankenneth/me@ba509eac628b0ae2a0817bb109ee4e500c9e7753 🚀

--- _next/static/JnRlI_ovltTXhMdd8p1Fm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ef9e6175cfb586ce.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
